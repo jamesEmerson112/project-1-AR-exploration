b8e42389b8868335e036b7015867d40d89537277
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2025-04-02 11:02:42 -0700

    attempt to run again

---
+++ AR_Exploration/AR_Exploration/AR_ExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_ExplorationApp.swift
+//  AR_Exploration
+//
+//  Created by James Emerson Vo on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AR_ExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
